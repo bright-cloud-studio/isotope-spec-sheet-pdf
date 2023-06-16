89991cc73013bbdd8f212ec0508e4e696fe8bebf
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-16 14:59:04 -0400

    Create config.php

---
+++ system/modules/isotope_spec_sheet_pdf/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope Spec Sheet PDF
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-spec-sheet-pdf
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+// Create Invoice - List's a psychologists entries in the Work Assignment sheet
+$GLOBALS['FE_MOD']['isotope_spec_sheet_pdf']['mod_isotope_spec_sheet_pdf']              = 'Bcs\Module\IsotopeSpecSheetPDF';
