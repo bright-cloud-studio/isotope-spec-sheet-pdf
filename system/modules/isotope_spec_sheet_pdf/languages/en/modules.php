50c4f0b329e27d7abb48abd49fe568603f89f206
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-16 15:17:07 -0400

    Create modules.php

---
+++ system/modules/isotope_spec_sheet_pdf/languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope Spec Sheet PDF
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-spec-sheet-pdf
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Front end modules */
+$GLOBALS['TL_LANG']['FMD']['mod_isotope_spec_sheet_pdf']  = array('Isotope Spec Sheet PDF', 'For testing the generation of PDF files on-the-fly');
