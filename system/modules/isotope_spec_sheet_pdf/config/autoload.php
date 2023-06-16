d39d0500fcb1f1f8b2c54570d6dfee21140ddffc
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-16 14:57:37 -0400

    Create autoload.php

---
+++ system/modules/isotope_spec_sheet_pdf/config/autoload.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope Spec Sheet PDF
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-spec-sheet-pdf
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Register the classes */
+ClassLoader::addClasses(array
+(
+    // Our Frontend Modules
+    'Bcs\Module\IsotopeSpecSheetPDFModule'    => 'system/modules/isotope_spec_sheet_pdf/library/Bcs/Module/IsotopeSpecSheetPDF.php'
+));
+
+/* Register the templates */
+TemplateLoader::addFiles(array
+(
+    'mod_isotope_spec_sheet_pdf'              => 'system/modules/isotope_spec_sheet_pdf/templates/modules'
+));
