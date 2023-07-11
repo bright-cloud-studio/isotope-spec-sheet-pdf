a6543c7d0bd873fbf6886d2187e16bd0af99796a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-11 09:56:36 -0400

    Update isotope_spec_sheet_pdf.js

--- system/modules/isotope_spec_sheet_pdf/assets/js/isotope_spec_sheet_pdf.js
+++ system/modules/isotope_spec_sheet_pdf/assets/js/isotope_spec_sheet_pdf.js
@@ -1,21 +1,25 @@
 
 // ON FINISHED LOADING
 $( document ).ready(function() {
-    
+
     // When the "Complete Work Assignment" button is clicked
-    $('input[name="complete_work_assignment"]').on("click", function(e) {
+    $('.mod_isotope_spec_sheet_pdf #generate_pdf').on("click", function(e) {
 
-      $.ajax({
-        url:'/system/modules/isotope_spec_sheet_pdf/assets/php/action.generate.pdf.php',
-        type:'POST',
-        success:function(result){
-          console.log("PDF: Generation Successful");
-        },
-        error:function(result){
-          console.log("PDF: Generation Failure");
-        }
-      });
+        console.log("PDF: Link clicked");
 
+        /*
+        $.ajax({
+            url:'/system/modules/isotope_spec_sheet_pdf/assets/php/action.generate.pdf.php',
+            type:'POST',
+            success:function(result){
+                console.log("PDF: Generation Successful");
+            },
+            error:function(result){
+                console.log("PDF: Generation Failure");
+            }
+        });
+        */
+    
     });
 
 });
