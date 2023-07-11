c6190e0407095976112e7fc6a863f1af616fcc9e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-11 15:47:37 -0400

    Update isotope_spec_sheet_pdf.js

--- system/modules/isotope_spec_sheet_pdf/assets/js/isotope_spec_sheet_pdf.js
+++ system/modules/isotope_spec_sheet_pdf/assets/js/isotope_spec_sheet_pdf.js
@@ -8,40 +8,29 @@ $(document).ready(function() {
         // Push a clicked message to the console so we know our event triggered
         console.log("PDF: Link clicked");
 
-        // Load our template file and pass it to the php script
-        var html_template;
-        $.get( "../templates/fabrics_with_variants.html", function( data ) {
-            html_template = data;
-            // the contents is now in the variable data
-            alert(html_template);
-        });
-
         // Ajax call is using a plugin "jquery-ajax-native.js" that allows it to get the returned data in the correct format, otherwise we were downloading a blank PDF
         $.ajax({
             dataType: 'native',
             url:'/system/modules/isotope_spec_sheet_pdf/assets/php/action.generate.pdf.php?testy=test123',
             type:'POST',
-            data: { html_template:html_template },
+            data: { our_data: "test 1 2 3" },
             xhrFields: {
                 responseType: 'blob'
             },
             success: function(data) {
                 
                 // Get our returned data as a blob
-                var blob=new Blob([data], {type: 'application/pdf'});
+                var blob = new Blob([data], {type: 'application/pdf'});
                 // Create a new link
-                var link=document.createElement('a');
+                var link = document.createElement('a');
                 // Set that link to our returned blob
-                link.href=window.URL.createObjectURL(blob);
+                link.href = window.URL.createObjectURL(blob);
                 // Set the file name
-                link.download="spec_sheet.pdf";
-                
+                link.download = "spec_sheet.pdf";
                 // for firefox
                 document.body.appendChild(link);
-                
                 // CLick our invisible link to initiate our download
                 link.click();
-                
                 // Push a success message to the console so we know things went well
                 console.log("PDF: Generation Successful");
             },
