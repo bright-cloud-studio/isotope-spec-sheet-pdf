e96f7cf7649b0314d03d20fb6377cd9fd29106e8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-11 12:52:26 -0400

    Update action.generate.pdf.php

--- system/modules/isotope_spec_sheet_pdf/assets/php/action.generate.pdf.php
+++ system/modules/isotope_spec_sheet_pdf/assets/php/action.generate.pdf.php
@@ -6,29 +6,34 @@
 	
 	// mandatory to get all of our composer goodness
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
-	
-	
-	
-	
-	// reference the Dompdf namespace
-	use Dompdf\Dompdf;
-	
-	// instantiate and use the dompdf class
-	$dompdf = new Dompdf();
 
+	// Use what we need
+	use Dompdf\Dompdf;
+    use Dompdf\Options;
+
+    // Create our options
+    $options = new Options();
+
+    // Set our options
+    $options->set("defaultFont", "Courier");
+
+	// Initialize dompdf with our options
+	$dompdf = new Dompdf($options);
+
+    // Build our HTML
     $html = "";
     $html .= '<h1>Spec Sheet</h1><br>';
     $html .= '<h2>Spec Sheet</h2><br>';
     $html .= '<h3>Spec Sheet</h3><br>';
     $html .= '<h4>Spec Sheet</h4><br>';
 
-
+    // Load our HTML into dompdf
 	$dompdf->loadHtml($html);
 	
-	// (Optional) Setup the paper size and orientation
+	// Set our paper size and orientation
 	$dompdf->setPaper('A4', 'portrait');
 	
-	// Render the HTML as PDF
+	// Render our PDF using the loaded HTML
 	$dompdf->render();
 	
 	// Output the generated PDF to Browser
