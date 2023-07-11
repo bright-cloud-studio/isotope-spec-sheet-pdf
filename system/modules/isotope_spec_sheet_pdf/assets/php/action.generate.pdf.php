a0c8cfe52d46f7d1263fad8f002342c9c3e59cd7
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-11 11:12:15 -0400

    Update action.generate.pdf.php

--- system/modules/isotope_spec_sheet_pdf/assets/php/action.generate.pdf.php
+++ system/modules/isotope_spec_sheet_pdf/assets/php/action.generate.pdf.php
@@ -5,7 +5,7 @@
 	/*************************************/
 	
 	// mandatory to get all of our composer goodness
-	require '../../vendor/autoload.php';
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
 	
 	
 	
@@ -15,15 +15,15 @@
 	
 	// instantiate and use the dompdf class
 	$dompdf = new Dompdf();
-	$dompdf->loadHtml('hello world');
+	$dompdf->loadHtml('<h1>hello world</h1>');
 	
 	// (Optional) Setup the paper size and orientation
-	$dompdf->setPaper('A4', 'landscape');
+	$dompdf->setPaper('A4', 'portrait');
 	
 	// Render the HTML as PDF
 	$dompdf->render();
 	
 	// Output the generated PDF to Browser
-	$dompdf->stream();
+	$dompdf->stream("spec_sheet.pdf");
 
 ?>
