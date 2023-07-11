f50aa1242baefdd86b737e8acc700164ea331771
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-07-11 10:05:23 -0400

    Update ModPDFGeneration.php

--- system/modules/isotope_spec_sheet_pdf/library/Bcs/Module/ModPDFGeneration.php
+++ system/modules/isotope_spec_sheet_pdf/library/Bcs/Module/ModPDFGeneration.php
@@ -1,63 +1,52 @@
 <?php
 
-  /**
-  * Bright Cloud Studio's Isotope Spec Sheet PDF
-  *
-  * Copyright (C) 2023 Bright Cloud Studio
-  *
-  * @package    bright-cloud-studio/isotope-spec-sheet-pdf
-  * @link       https://www.brightcloudstudio.com/
-  * @license    http://opensource.org/licenses/lgpl-3.0.html
-  **/
+    /**
+    * Bright Cloud Studio's Isotope Spec Sheet PDF
+    *
+    * Copyright (C) 2023 Bright Cloud Studio
+    *
+    * @package    bright-cloud-studio/isotope-spec-sheet-pdf
+    * @link       https://www.brightcloudstudio.com/
+    * @license    http://opensource.org/licenses/lgpl-3.0.html
+    **/
 
 
-namespace Bcs\Module;
+    namespace Bcs\Module;
 
-class ModPDFGeneration extends \Contao\Module
-{
-
-  /**
-  * Template
-  * @var string
-  */
-  protected $strTemplate = 'mod_isotope_spec_sheet_pdf';
-
-  /**
-  * Initialize the object
-  *
-  * @param \ModuleModel $objModule
-  * @param string       $strColumn
-  */
-  public function __construct($objModule, $strColumn='main')
-  {
-    parent::__construct($objModule, $strColumn);
-  }
-
-  /**
-  * Display a wildcard in the back end
-  * @return string
-  */
-  public function generate()
-  {
-    if (TL_MODE == 'BE')
+    class ModPDFGeneration extends \Contao\Module
     {
-    $objTemplate = new \BackendTemplate('be_wildcard');
+        
+        protected $strTemplate = 'mod_isotope_spec_sheet_pdf';
+    
+        public function __construct($objModule, $strColumn='main')
+        {
+            parent::__construct($objModule, $strColumn);
+        }
 
-    $objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['isotope_spec_sheet_pdf'][0]) . ' ###';
-    $objTemplate->title = $this->headline;
-    $objTemplate->id = $this->id;
-    $objTemplate->link = $this->name;
-    $objTemplate->href = 'contao/main.php?do=themes&table=tl_module&act=edit&id=' . $this->id;
+        public function generate()
+        {
+            if (TL_MODE == 'BE')
+            {
+                $objTemplate = new \BackendTemplate('be_wildcard');
+        
+                $objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['isotope_spec_sheet_pdf'][0]) . ' ###';
+                $objTemplate->title = $this->headline;
+                $objTemplate->id = $this->id;
+                $objTemplate->link = $this->name;
+                $objTemplate->href = 'contao/main.php?do=themes&table=tl_module&act=edit&id=' . $this->id;
+        
+                return $objTemplate->parse();
+            }
+        
+            return parent::generate();
+        }
+    
+        /* Generate the module */
+        protected function compile()
+        {
+            // add our js
+            $rand_ver = rand(1,9999);
+            $GLOBALS['TL_BODY'][] = '<script src="system/modules/isotope_spec_sheet_pdf/assets/js/isotope_spec_sheet_pdf.js?v='.$rand_ver.'"></script>';
+        }
 
-    return $objTemplate->parse();
-  }
-
-  return parent::generate();
-  }
-
-  /* Generate the module */
-  protected function compile()
-  {
-  }
-  
-} 
+    } 
